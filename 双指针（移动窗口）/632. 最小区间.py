f16ea449613469b3ff52a33e18c31390b9dac365
2892211452 <54618163+2892211452@users.noreply.github.com>
2020-08-03 11:16:03 +0800

    Update 632. 最小区间.py

--- 双指针（移动窗口）/632. 最小区间.py
+++ 双指针（移动窗口）/632. 最小区间.py
@@ -17,10 +17,41 @@
 # -105 <= 元素的值 <= 105
 # 对于使用Java的用户，请注意传入类型已修改为List<List<Integer>>。重置代码模板后可以看到这项改动。
 
-
-class Solution(object):
+class Solution:
     def smallestRange(self, nums):
-        """
-        :type nums: List[List[int]]
-        :rtype: List[int]
-        """
+        from queue import PriorityQueue  # 最小堆，里面元素自动排列，     ！！！！！！！！！！！！！！！！
+        q = PriorityQueue()
+        N = len(nums)
+        INF = 10 ** 9
+        maxv = -INF
+        start, end = -INF, INF
+
+        #先把所有序列得头节点放进去， 也就是最小得放进去    i代表在哪个数组，  j代表第几个
+        for i in range(N):
+            q.put((nums[i][0], i, 0))
+            maxv = max(maxv, nums[i][0])
+
+        # 然后利用数据结构队列，进行遍历
+        while q.qsize() == N:
+
+            #一个一个遍历
+            v, i, j= q.get()
+            print(v, i,j)
+
+
+
+            # 是否需要更新
+            if maxv - v < end - start:
+                start, end = v, maxv
+
+
+            # 是否可以将数据进栈
+            if j + 1 < len(nums[i]):
+                nv = nums[i][j + 1]
+                q.put((nv, i, j + 1))
+                maxv = max(maxv, nv)
+
+        return [start, end]
+
+ans  = Solution.smallestRange(None, [[4,10,15,24,26],[0,9,12,20],[5,18,22,30]])
+print(ans)
