ac3dea29e8eb06036d86b132794360fc46594025
xingzhe <54618163+2892211452@users.noreply.github.com>
2022-05-29 15:00:41 +0800

    更新c++算法头文件模板

---
+++ 比赛/他.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>    //通用算法
+#include <deque>//双端队列容器
+#include <list>//线性列表容器
+#include <map>//映射容器
+#include <iostream>//基本输入输出流
+#include <queue>//队列容器
+#include <set>//集合容器
+#include <stack>//堆栈容器　　　
+#include <string>  //字符串类
+#include <vector> //动态数组容器
+#include <cmath>       //数学函数类
+
+using namespace std;
+
+
+class Solution {
+public:
+    map<int,int>count;
+    bool digitCount(string num) {
+        for(int i=0; i < num.length(); i++){
+            int now = num[i] - '0';
+
+        }
+    }
+};
